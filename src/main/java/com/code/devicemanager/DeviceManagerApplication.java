a76e2f5b5c15d1b168baac7fbcb419fe68eaae00
Nuno Pinhao <nunopinhao97@gmail.com>
2025-08-08 10:21:46 +0100

    #NP# Add DeviceManagerApplication and update .gitignore

---
+++ src/main/java/com/code/devicemanager/DeviceManagerApplication.java
@@ -0,0 +1,10 @@
+package com.code.devicemanager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagerApplication.class, args);
+    }
+}
